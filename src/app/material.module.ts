24ca9a224482b39783e908ded00aebcd18e36866
suwitna <44050482+suwitna@users.noreply.github.com>
2019-07-01 01:40:21 +0700

    ament codes
    add navigation menu

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,10 +1,33 @@
 import {NgModule} from '@angular/core';
-import {MatBadgeModule} from '@angular/material/badge';
-import {MatButtonModule} from '@angular/material/button';
+import {
+    MatBadgeModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatListModule,
+    MatTabsModule
+} from '@angular/material';
 
 @NgModule({
-    imports: [MatBadgeModule, MatButtonModule],
-    exports: [MatBadgeModule, MatButtonModule]
+    imports: [
+        MatBadgeModule,
+        MatButtonModule,
+        MatSidenavModule,
+        MatToolbarModule,
+        MatIconModule,
+        MatListModule,
+        MatTabsModule
+    ],
+    exports: [
+        MatBadgeModule,
+        MatButtonModule,
+        MatSidenavModule,
+        MatToolbarModule,
+        MatIconModule,
+        MatListModule,
+        MatTabsModule
+    ]
 })
 
-export class MaterialModule {}
+export class MaterialModule { }
